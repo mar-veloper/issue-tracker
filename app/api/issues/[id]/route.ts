236b9bd2b111ccd88dbcc61a831ac86b17173336
mar-veloper <jonmar21yan@gmail.com>
2023-12-29 19:40:52 +0100

    Add: update sepcific issue api

---
+++ app/api/issues/[id]/route.ts
@@ -0,0 +1,36 @@
+import issueSchema from '@/app/schemas/issue.schema'
+import prisma from '@/prisma/client'
+import { NextRequest, NextResponse } from 'next/server'
+
+interface Args {
+  params: {
+    id: string
+  }
+}
+
+export async function PUT(request: NextRequest, { params: { id } }: Args) {
+  const body = await request.json()
+
+  const validation = issueSchema.safeParse(body)
+
+  if (!validation.success) {
+    return NextResponse.json(
+      { errors: validation.error.format() },
+      { status: 400 }
+    )
+  }
+  const issue = await prisma.issue.findUnique({
+    where: { id },
+  })
+
+  if (!issue) {
+    return NextResponse.json({ errors: 'Issue not found' }, { status: 404 })
+  }
+
+  const updatedIssue = await prisma.issue.update({
+    where: { id },
+    data: { title: body.title, description: body.description },
+  })
+
+  return NextResponse.json(updatedIssue)
+}
