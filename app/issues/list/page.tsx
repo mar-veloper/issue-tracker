f221f46d15ab6d1ed90093ad6a7dd12c00a057aa
mar-veloper <jonmar21yan@gmail.com>
2024-01-01 02:40:12 +0100

    Add: Table sorting by column header

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -1,28 +1,71 @@
 import { IssueStatusBadge, Link } from '@/app/components'
 import prisma from '@/prisma/client'
-import { Table } from '@radix-ui/themes'
+import { Flex, Table } from '@radix-ui/themes'
 import IssueActions from './IssueActions'
 import routes from '@/app/routes'
-import { Status } from '@prisma/client'
+import { Issue, Status } from '@prisma/client'
+import NextLink from 'next/link'
+import { ArrowDownIcon, ArrowUpIcon } from '@radix-ui/react-icons'
 
 interface Props {
   searchParams: {
     status: Status
+    sort: keyof Issue
+    sortOrder: 'asc' | 'desc'
   }
 }
 
+interface Column {
+  label: string
+  value: keyof Issue
+  className?: string
+}
+
 const IssuesPage = async ({ searchParams }: Props) => {
+  const columns: Column[] = [
+    {
+      label: 'Issue',
+      value: 'title',
+    },
+    {
+      label: 'Status',
+      value: 'status',
+      className: 'hidden md:table-cell',
+    },
+    {
+      label: 'Created',
+      value: 'createdAt',
+      className: 'hidden md:table-cell',
+    },
+  ]
+
   const statuses = Object.values(Status)
   const status = statuses.includes(searchParams.status)
     ? searchParams.status
+    : undefined
+
+  const isSortedByTitle = searchParams.sort === 'title'
+  const orderByKey = searchParams.sort || 'createdAt'
+  const defaultSortOrder = isSortedByTitle ? 'asc' : 'desc'
+
+  const activeSortOrder = ['asc', 'desc'].includes(searchParams.sortOrder)
+    ? searchParams.sortOrder
     : undefined
 
   const issues = await prisma.issue.findMany({
     where: {
       status,
     },
-    orderBy: { status: 'desc' },
+    orderBy: {
+      [orderByKey]: activeSortOrder || defaultSortOrder,
+    },
   })
+
+  const isSortAscending = searchParams.sortOrder === 'asc'
+
+  const toggleSortOrder = () => (isSortAscending ? 'desc' : 'asc')
+  const renderArrowIcon = () =>
+    isSortAscending ? <ArrowUpIcon /> : <ArrowDownIcon />
 
   return (
     <div>
@@ -30,13 +73,34 @@ const IssuesPage = async ({ searchParams }: Props) => {
       <Table.Root variant="surface">
         <Table.Header>
           <Table.Row>
-            <Table.ColumnHeaderCell>Issue</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell className="hidden md:table-cell">
-              Status
-            </Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell className="hidden md:table-cell">
-              Created
-            </Table.ColumnHeaderCell>
+            {columns.map((column) => {
+              const isSortActive = searchParams.sort === column.value
+              const sortOrder = isSortActive
+                ? toggleSortOrder()
+                : defaultSortOrder
+              return (
+                <Table.ColumnHeaderCell
+                  key={column.value}
+                  className={column.className}
+                >
+                  <NextLink
+                    href={{
+                      query: {
+                        ...searchParams,
+                        sort: column.value,
+                        sortOrder,
+                      },
+                    }}
+                  >
+                    <Flex gap="1" align="center">
+                      {column.label}
+
+                      {isSortActive && renderArrowIcon()}
+                    </Flex>
+                  </NextLink>
+                </Table.ColumnHeaderCell>
+              )
+            })}
           </Table.Row>
         </Table.Header>
         <Table.Body>
